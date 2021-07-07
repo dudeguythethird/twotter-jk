cf4160c7e28527b301af7b3d6ddc03d2ad88bf00
Joseph Keable <keablejoseph@gmail.com>
2021-07-07 13:41:01 +0100

    adds user, tweets and tweet favouriting functionality

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="user-profile">
+      <div class="user-profile__user-panel">
+          <h1 class="user-profile__username">@{{ user.username }}</h1>
+          <div class="user_profile__admin-badge" v-if="user.isAdmin">
+              Admin
+          </div>
+          <div class="user-profile__follower-count">
+              <strong>Followers:</strong> {{ followers }}
+          </div>
+      </div>
+    <div class="user-profile__twoots-wrapper">
+      <TwootItem 
+        v-for="twoot 
+        in user.twoots" 
+        :key="twoot.id" 
+        :username="user.username" 
+        :twoot="twoot" 
+        @favourite="toggleFavourite" 
+      />
+    </div> 
+  </div>
+</template>
+
+<script>
+import TwootItem from "./TwootItem";
+
+export default {
+  name: 'UserProfile',
+  components: { TwootItem },
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'josephkeable',
+        firstName: 'Joseph',
+        lastName: 'Keable',
+        email: 'keablejoseph@gmail.com',
+        isAdmin: true,
+        twoots: [
+          { id: 1, content: 'Twotter is dope!'},
+          { id: 2, content: 'Dont forget, the earth is flat!'}
+        ]
+      }
+    }
+  },
+  watch: {
+    followers(newFolowerCount, oldFollowerCount) {
+      if(oldFollowerCount < newFolowerCount) {
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++
+    },
+    toggleFavourite(id) {
+      console.log(`Favourited Twoot: ${id}`)
+    }
+  },
+  mounted() {
+    this.followUser();
+  }
+}
+</script>
+
+<style>
+.user-profile {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100%;
+    grid-gap: 50px;
+    padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+}
+
+.user_profile__admin-badge {
+    background: rebeccapurple;
+    color: white;
+    border-radius: 5px;
+    margin-right: auto;
+    padding: 0 10px;
+    font-weight: bold;
+}
+
+h1 {
+    margin: 0;
+}
+
+.user-profile__twoots-wrapper {
+  display: grid;
+  grid-gap: 10px;
+}
+
+</style>
